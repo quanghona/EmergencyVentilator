8dc88ac4115182d05bd8bbfcbe5fa12a0439cee0
Quang Ly <lyhonquang@gmail.com>
2020-04-08 18:58:21 +0700

    [Library] Correct pressure conversion fomula

--- Core/Src/sensor.c
+++ Core/Src/sensor.c
@@ -15,6 +15,8 @@
 static SensorData_t data = {0.0F, 0.0F, 0.0F, 0.0F};
 static uint32_t data_raw[4] = {0, 0, 0, 0};
 
+const float KPA_TO_CMH2O_FACTOR = 0.0980665f;
+
 /****************************Function definitions*****************************/
 
 /******************************************************************************
@@ -37,12 +39,16 @@ void Sensor_UpdateValue(uint32_t* pui32Raw)
 /******************************************************************************
  * @brief Convert and return sensor data structure instance
  * 
+ * @details The pressure conversion fomula follow the Transfer funtion define in
+ * MPXV7025's datasheet file.
  * @return SensorData_t* pointer to the data instance
+ * 
+ * @link https://www.mouser.com/datasheet/2/302/MPXV7025-1127333.pdf
 *****************************************************************************/
 SensorData_t* Sensor_GetData(void)
 {
     /* Convert data */
-    data.pressure = (22 * data_raw[0] - 68250) / 2457.0f;
+    data.pressure = (22 * data_raw[0] - 68250) / 2457.0f * KPA_TO_CMH2O_FACTOR;
     data.angle = data_raw[1];
     data.ris = data_raw[2];
     data.lis = data_raw[3];
